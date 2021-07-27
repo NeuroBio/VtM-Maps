3d00eda2e34c901dd0d7a4800284c05e106b99cc
Cristina Robinson <NeuroBio@users.noreply.github.com>
2021-07-26 20:57:12 -0400

    get boot strap and leaflet happied

---
+++ static/js/map.js
@@ -0,0 +1,10 @@
+var map = L.map('pheonix-map').setView([33.4484, -112.0740], 9);
+streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/dark-v10',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: apiKey
+});
+streets.addTo(map);
