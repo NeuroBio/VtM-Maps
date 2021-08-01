7bdc998cb8b13bebd2798a90edd845483bf973d3
Cristina Robinson <NeuroBio@users.noreply.github.com>
2021-08-01 14:50:20 -0400

    add more places

--- static/js/char.js
+++ static/js/char.js
@@ -16,5 +16,7 @@ chars = {
 
 havens = [
     makeChar('Stefan', [33.4506, -112.0741], 'A rented room at the San Carlos Hotel.  Stefan benefits from the hotel security cameras to protect himself and his endless horde of stolen shiny objects in relative comfort.  Naturally, he also steals from the other guests.'),
-    makeChar('Irfan', [33.48648969915542, -112.08362731511266], 'A studio apartment rented at the Metropolitan Apartments.  There is little security, no luxury, and this area is only tentatively held as Camarilla turf.  It was historically Sabbat, and the Sabbat is aggressively working to reclaim the area.')
-]
+    makeChar('Irfan', [33.48648969915542, -112.08362731511266], 'A studio apartment rented at the Metropolitan Apartments.  There is little security, no luxury, and this area is only tentatively held as Camarilla turf.  It was historically Sabbat, and the Sabbat is aggressively working to reclaim the area.'),
+    makeChar('Gree', [33.363814263278435, -112.03995605332327], 'A perpetually under construction frame of what theoretically could be a mansion.  Gree’s dream house project is unlikely to ever be finished, because he keep changes him mind about what the perfect house looks like.  Provides next to no security outside of its remote location, because none of the walls or rooms are finished.'),
+    
+];
--- static/js/places.js
+++ static/js/places.js
@@ -18,5 +18,20 @@ places = [
         name: 'The Freeport McMoran Building',
         location: [33.4517, -112.0730],
         desc: 'By day, it is the headquarters of the largest publicly traded copper producer.  By night, it is the headquarters of the Phoenix Camarilla under Prince Santana.  The first time Thaddeus and Iz entered the building to drop off a package from Hartstone Prince Grayson to Prince Santana.  Along the way, Thaddeus accidently made the acquaintance of the rich blood-leach Dmitri, who would later participate in the coup to take over the Hartstone Camarilla.  On their second visit, the fledglings asked the Thinblood receptionist, TimJohn, about the possibility of meeting members of the Tremere Chantry in Phoenix, which put Thaddeus into communication with TimJohn’s Tremere sister, Ava.'
+    },
+    {
+        name: 'University of Phoenix',
+        location: [33.4164117242527, -112.01359445228233],
+        desc: 'The university Iz studied programming and comparative religion at prior to the embrace.'
+    },
+    {
+        name: 'Cuit of Phoenix Police Station',
+        location: [33.46081926752634, -112.07388522221011],
+        desc: 'The station where Thaddeus’ older sister, Anna, works.'
+    },
+    {
+        name: 'Desert Botanical Garden',
+        location: [33.46176108751268, -111.94444676865537],
+        desc: 'The location of Thaddeus and Dmitri’s first date.  They two enjoyed looking at cactuses until Thaddeus’ sire, Trevor, ran in screaming about how dates are a waste of time and Dmitri is a bad influence.  The date was cut short.  Thaddeus was infinitely grateful for this in the long run.'
     }
 ];
