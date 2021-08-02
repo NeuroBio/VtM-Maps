c5c8ec7bd9522ff35bdcf9d17891dbdffbd00ee8
Cristina Robinson <NeuroBio@users.noreply.github.com>
2021-08-02 14:03:29 -0400

    set up to display characters

--- static/js/chars.js
+++ static/js/chars.js
@@ -1,6 +1,24 @@
 chars = {
     Stefan: {
+        fullName: 'Stefan Müller',
+        player: 'The Storyteller',
+        concept: 'Crow-kavian',
+        ambition: 'Wants Prince Santana\'s Pearl Necklace',
+        clan: 'Malkavian',
+        childer: 'N/A',
+        sire: 'Rowan Ward',
+        keyNote: 'A good-natured kleptomaniac who hates birds',
+        history: 'Stefan came from a once wealthy German family and joined WW1 to regain some family pride. He was not happy with trench warfare and backed away from the idea of any further military pursuits when the war ended.  He picked up jewelry-craft to maintain ties to the elite and make a name for himself. When WW2 was on the brink of erupting, Stefan decided NO WAY, and fled to America. While still getting his bearings, he ran into Rowan, who wanted specialty silverware. An odd request, but Rowan paid well, so Stefan went for it.  Rowan very quicky developed feelings for Stefan and brought him over, thinking everything would be fine.  I.e. without considering to ask if Stefan liked him back. Stefan did not, but he made for a respectful childe.  He stayed in Hartstone and helped take care of Rowan with Aria for decades.  That ended when Stefan pissed off Garren by stealing Garren\’s nice things one too many times.  He was banned from Hartstone in the 1970s and has been with the Phoenix Camarilla since.  Recently, Stefan became the local Malkavian leader, because their previous Primogen, Dorian, left for the Gehenna Wars.'
+    },
+    x: {
         fullName: '',
-
+        player: '',
+        concept: '',
+        ambition: '',
+        clan: '',
+        childer: '',
+        sire: '',
+        keyNote: '',
+        history: ''
     }
 };
--- static/js/htmlControls.js
+++ static/js/htmlControls.js
@@ -1,3 +1,4 @@
+// Control map view
 function switchMap() {
     if (d3.select(this).property('value') === 'true') {
         d3.select('#hartstone-map').style('display', 'block');
@@ -11,3 +12,29 @@ function switchMap() {
 }
 
 d3.selectAll('input[class="map-select"]').on('change', switchMap);
+
+// set active character
+function setActiveChar(charName) {
+    const char = chars[charName];
+    // set main info
+    Object.keys(char).forEach(key =>
+        d3.select(`#${key}`).text(`${char[key]}`)
+    );
+    // set image
+    d3.select('#portrait').attr('src', `static/images/portraits/${charName}.png`)
+}
+
+// add characters
+function updateEntries(people) {
+    if (people === 'true') {
+        const select = d3.select('#pick-element')
+        select.html('');
+        Object.values(chars).forEach(char => 
+            select.append("option")
+            .text(char.fullName)
+            .property('value', char.fullName));
+    }
+}
+
+updateEntries('true');
+setActiveChar('Stefan');
